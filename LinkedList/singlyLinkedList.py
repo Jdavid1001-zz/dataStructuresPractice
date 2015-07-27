6746703a942940c68293852ac063ec92aeca2ff6
Juan David <juandominguez2017@u.northwestern.edu>
2015-07-26 22:55:56 -0700

    Beginnig the linked lists portion

---
+++ LinkedList/singlyLinkedList.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import sys
+sys.path.append('/Users/JuanDa/Documents/Spyder Workspace/Interview Prep/dataStructures/Hash')
+from MyHashBase import StringBucket
+
+class SuperNode:
+    def __init__(self, data):
+        self
+    
+class SinglyLinkedList(StringBucket):
+    def __init__(self):
+        super(self.__class__, self).__init__()
+        
